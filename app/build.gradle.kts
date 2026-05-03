cb18b4b770fe8242742f22083527664c810b0bfc
waddahbakil <42424111+waddahbakil@users.noreply.github.com>
2026-05-03 19:44:55 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,10 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.waddah.pharmacypdf"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.waddah.pharmacypdf"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-    
-    buildFeatures {
-        viewBinding = true
-    }
-    
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    // مكتبة توليد PDF
+    implementation("com.itextpdf:itext7-core:7.2.5")
 }
